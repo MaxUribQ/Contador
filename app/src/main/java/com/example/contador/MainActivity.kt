f349edeaa5f25579ffbad3967f521524e1dbf89b
Maximiliano Uribe Quintero <m.uribe23@info.uas.edu.mx>
2026-03-09 21:54:59 -0700

    Añadimos la funcion de orientacion de pantalla a la Tarea

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    var contador = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -13,7 +16,11 @@ class MainActivity : AppCompatActivity() {
 
         val botonContador = findViewById<Button>(R.id.btnContador)
         val botonReiniciar = findViewById<Button>(R.id.btnReiniciar)
-        var contador = 0
+
+        if(savedInstanceState != null){
+            contador = savedInstanceState.getInt("contador")
+            botonContador.text = contador.toString()
+        }
 
         botonContador.setOnClickListener {
 
@@ -28,6 +35,11 @@ class MainActivity : AppCompatActivity() {
             botonContador.text = "0"
 
         }
-
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("contador", contador)
+    }
+
 }
